f3cab55d8cde7070e77eb77d3c2260de242dbda0
Vero Rodriguez <vrodrig@thoughtworks.com>
2022-11-08 18:02:00 -0500

    [Cris, Vero] Prepare green test

--- src/main/java/com/workshop/tddworkshop/controllers/BecaEstudianteController.java
+++ src/main/java/com/workshop/tddworkshop/controllers/BecaEstudianteController.java
@@ -1,13 +1,25 @@
 package com.workshop.tddworkshop.controllers;
 
+import com.workshop.tddworkshop.controllers.dto.StudentDTO;
+import com.workshop.tddworkshop.model.Student;
+import com.workshop.tddworkshop.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BecaEstudianteController {
 
-    @GetMapping(value ="v1/becas/alumno/")
-    public String getStudentInfo(){
-        return "Fernanda";
+    @Autowired
+    private StudentRepository studentRepository;
+
+
+    @GetMapping(value ="v1/becas/alumno/{id}")
+    public StudentDTO getStudentInfo(@PathVariable("id") long id){
+
+        Student student = studentRepository.findById(id);
+        StudentDTO studentDTO = new StudentDTO(student.getId(), student.getName());
+        return studentDTO;
     }
 }
